27dab6e4f28769ae093ef384d8275f0370193d0f
partha235 <partha235@gmail>
2022-03-03 16:28:59 +0530

    current files

---
+++ cpp_prog/for_cpp.cpp
@@ -0,0 +1,11 @@
+/* this code is based on "for" loop where the loop run for certain condition. */
+/* before this you have to know about increment (i++) & decrement (i--) to run this loop */
+
+#include<iostream>
+using namespace std;
+main(){
+    int x;
+    for(x=0;x<=10;x--){
+        cout<<"saga\n";
+    }
+}
