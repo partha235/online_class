a2bec683bd6a39fa6fd2989cb9426c8632abfef6
partha235 <partha235@gmail>
2022-03-30 12:58:11 +0530

    current working

---
+++ python/if_stat.py
@@ -0,0 +1,4 @@
+a=10
+b=int(input("enter b"))
+if a>=b:
+    print(a>=b) #return true 
---
+++ python/vari_assig.py
@@ -0,0 +1,4 @@
+a=float(input("enter a number"))
+b=float(input("enter b"))
+
+print("area of cone",(1/2)*a*b)
