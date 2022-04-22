7b80592009db3db098b12f7e1d94f94a4a7b1a3c
Parthasarathy <parthaa235@gmail.com>
2022-04-22 17:42:37 +0530

    current working

--- cpp_prog/add.cpp
+++ cpp_prog/add.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 main(){
     system("cls");
-    int a=10,b=5;
+    int a=10,b;
+    cout<<"ent b";
+    cin>>b;
     cout<<"a+b="<<a+b<<endl;
 }
---
+++ python/nes_for.py
@@ -0,0 +1,5 @@
+x=0
+for x in range(2):
+    for x in range(0,4):
+        print(x,' saga')
+    print(x,"hi")
