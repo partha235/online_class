5aef13369f1a7c197b383cf6dbe14a5f779a88eb
Parthasarathy <parthaa235@gmail.com>
2022-11-19 19:12:06 +0530

    working

--- cpp_prog/nes_if.cpp
+++ cpp_prog/nes_if.cpp
@@ -1,3 +1,6 @@
+/* this program is an example of nested if statement.
+This program runs with age of the person as int and process it for that he can vote or not and also he/she belongs to 
+senior citizenship or not.*/
 #include"iostream"
 using namespace std;
 int age;
