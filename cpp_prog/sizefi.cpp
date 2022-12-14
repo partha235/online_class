f0667e84a69dc47146789f8a425ac7ff79b9136a
Parthasarathy <parthaa235@gmail.com>
2022-12-15 02:03:55 +0530

    working

---
+++ cpp_prog/sizefi.cpp
@@ -0,0 +1,9 @@
+// this program is to find the size of an object.
+
+#include"iostream"
+using namespace std;
+main(){
+    cout<<"size of saga: "<<sizeof("saga")<<endl;
+    cout<<"size of 23: "<<sizeof(23)<<endl;
+    cout<<"size of $: "<<sizeof("$")<<endl;
+}
