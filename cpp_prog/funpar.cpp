503b2a84a68a345b33a47e4ab16f6921571101d9
Parthasarathy <parthaa235@gmail.com>
2022-12-29 11:37:09 +0530

    working

---
+++ cpp_prog/funpar.cpp
@@ -0,0 +1,17 @@
+#include"iostream"
+using namespace std;
+void circle(int r){
+    cout<<"area of circle "<<3.14*r*r<<endl;
+}
+void square(int a){
+    cout<<"area of square"<<4*a<<endl;
+}
+void cone(int b, int h){
+    cout<<"area of cone "<<(1*b*h)/2<<endl;
+}
+main(){
+    system("cls");
+    cone(2,12);
+    circle(5);
+    square(16);
+}
