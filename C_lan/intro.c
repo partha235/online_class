85ed245f1ba8453d7bfea7980b5d58d620d962d2
Parthasarathy <parthaa235@gmail.com>
2022-12-30 20:32:01 +0530

    updating

--- C_lan/intro.c
+++ C_lan/intro.c
@@ -1,7 +1,4 @@
 #include"stdio.h"
 void main(){
-    char x[]="saga@23",pass[8];
-    printf("enter");
-    scanf("%s",pass);
-    printf("%s\n",pass);
+    printf("saga"); //" *.c "
 }
--- cpp_prog/intro_cpp.cpp
+++ cpp_prog/intro_cpp.cpp
@@ -1,8 +1,8 @@
-#include<iostream>
+#include"iostream"
 using namespace std;
 
 main(){
-	system("cls");
- cout<<"saga";
+	system("cls"); //" 8.cpp"
+ cout<<"bps_explore";
 }
 	
