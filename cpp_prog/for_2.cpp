0623df9bbfbf72fb32daa6aab73183cce7b2c3b3
partha235 <parthaa235@gmail.com>
2023-11-07 16:02:07 +0530

    updated

---
+++ cpp_prog/for_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+   int nums[] = {4, 9, 6, 72, 31, 44};
+   string x = "bps_explore";
+ 
+   for (int num: nums) {
+    cout<< num<<endl;
+   }
+   for(char y: x){
+    cout<<y<<endl;
+   }
+}
