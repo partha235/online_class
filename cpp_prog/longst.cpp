d0326f9e3ec35df2f7a02e1fd47886e7d93004d7
partha235 <parthaa235@gmail.com>
2023-11-08 14:07:47 +0530

    working

--- cpp_prog/longst.cpp
+++ cpp_prog/longst.cpp
@@ -3,6 +3,11 @@
 using namespace std;
 
 int main() {
-  long double x = 3.14159265358979323846;
-  cout << "x = " << x << endl;
+    long double x = 3.14159265358979323846;
+    long y = 1234567890;
+    long z = -1234567890;
+
+    cout << "x = " << x << endl;
+    cout << "y = " << y << endl;
+    cout << "z = " << z << endl;
 }
