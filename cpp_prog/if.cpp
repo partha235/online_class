825b3922d396fc2bf3d6c3ce91e66a702bebe812
partha235 <parthaa235@gmail.com>
2024-03-18 10:46:03 +0530

    updated

--- cpp_prog/if.cpp
+++ cpp_prog/if.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 using namespace std;
-main(){
+int main(){
     string pas,pas_s;
     cout<<"enter password";
     cin>>pas_s;
-    system("cls");
+    system("clear");
     //now the password is saved in mobile storage
     cout<<"enter password for open mobile";
     cin>>pas;
-    system("cls");
+    system("clear");
     if (pas==pas_s){
         cout<<"mobile open"<<endl;
     }
