64221ee1e577189852527beb2c3a6959348bb0b7
partha235 <parthaa235@gmail.com>
2024-03-26 12:12:15 +0530

    updated

---
+++ cpp_prog/test1.cpp
@@ -0,0 +1,11 @@
+# include<iostream>
+using namespace std;
+int main(){
+  system("clear");
+    int a,b;
+    cout<<"enter a"<< endl;
+    cin>>a;
+    cout<<"enter b"<< endl;
+    cin>>b;
+    cout<<"a+b= "<<a+b<< endl;
+}
