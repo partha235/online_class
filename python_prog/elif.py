7a2b9b710b3950f6822d116dad7a5a0908940c80
partha235 <parthaa235@gmail.com>
2024-04-12 12:26:32 +0530

    update

--- python_prog/elif.py
+++ python_prog/elif.py
@@ -4,5 +4,13 @@ if a>=b:
     print(a>=b) #return true 
 elif b>=20:
     print("b>20")
+# elif b>=50:
+#     print(b>=50)
 else:
     print("false")
+
+# if b>=20:
+#     print("b>=20")
+
+# if b>=50:
+#     print("b>=50")
