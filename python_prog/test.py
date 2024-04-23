89e49adfafecd368b8d80ece4425af6381617893
partha235 <parthaa235@gmail.com>
2024-04-23 19:01:41 +0530

    updated

--- python_prog/test.py
+++ python_prog/test.py
@@ -1,11 +1,7 @@
-# make a calculator to add n number of digits and stop when input is "=".
-# 0,1,2,3........n
-add_v=0
-while True:
-    x=input("enter number")
-    if x=="=":
-        break
-    add_va=int(x)+int(add_v)
-    add_v=add_va
+import turtle as t
+import random as r
+x=["truth","dare"]
 
-print(add_v)
+for i in range(2):
+    y=r.choice(x)
+    print(y)
