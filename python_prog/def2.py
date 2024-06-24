04f03c2fdb3a4af3d1f1a8dab19008132b464030
partha235 <parthaa235@gmail.com>
2024-06-25 01:09:16 +0530

    updated

--- python_prog/def2.py
+++ python_prog/def2.py
@@ -1,21 +1,16 @@
 def area_circle(r,pi=3.14):
     print('area of circle= ',pi*(r**2))
 
-# area_circle(r=2,pi=2)
-
-
 def per_detail(name,work,salary=None):
     print("{} is working as {} and having salary of {}".format(name,work,salary))
-
-
 
 def str_mul(n:int,w:str):
     print("you have enter {} to print for {} times".format(w,n))
     w="{}\n".format(w)
     print(w*n)
 
-# area_circle(5)
-# str_mul(5,"mathes")
-# per_detail("bps","teaching")
+# area_circle(r=5)
+# area_circle(r=5,pi=2)
+# str_mul(10,"suveatha")
+# per_detail("bps","teacher")
 # per_detail("bps","electronic",50000)
-
--- python_prog/def_re.py
+++ python_prog/def_re.py
@@ -1,10 +1,10 @@
 def area_circle(r):
     print('area of circle= ',3.14*(r**2))
 
-def ac_re(r):
+def area_cir_re(r):
     ac=3.14*(r**2)
-    return "area of circle= ",ac
+    return "area of circle= ",ac 
 
 # area_circle(5)
-# ac_re(5)
-# print(ac_re(5))
+# area_cir_re(5)
+# print(area_cir_re(5))
